a39e9451946539819af42836bcf61f3af65b44cf
Ruih11 <72733709+Ruih11@users.noreply.github.com>
2020-10-27 14:00:58 +0800

    fix step counter on main screen

--- VP step counter/Assets/MainPage.cs
+++ VP step counter/Assets/MainPage.cs
@@ -2,16 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using PedometerU;
 
 public class MainPage : MonoBehaviour
 {
-    public GameObject stepsDisplay;
-   
-   
+    public Text stepsDisplay;
+    private int initialMainSteps;
+    private int mainSteps;
+    private Pedometer pedometer;
+
+    private void Start()
+    {
+        initialMainSteps = PlayerPrefs.GetInt("steps", 0);
+        pedometer = new Pedometer(OnStep);
+    }
 
     // Update is called once per frame
-    void Update()
-    {
-        stepsDisplay.GetComponent<Text>().text = "Steps: " + SceneData.steps;
+    
+    private void OnStep(int steps, double distance)
+        {
+        // Display the values // Distance in feet
+            steps += initialMainSteps;
+            stepsDisplay.text = "Steps: " + steps.ToString();
+        PlayerPrefs.SetInt("steps", steps);
+
+
     }
+
+    
+
+        private void OnDisable()
+        {
+            // Release the pedometer
+            pedometer.Dispose();
+            pedometer = null;
+        }
+  
 }
